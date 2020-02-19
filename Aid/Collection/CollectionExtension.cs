524338e5c164b0122f027a94fc85d312c0209557
Jiří <Jiri.Cepelka@Volny.cz>
2020-02-19 23:10:14 +0100

    Fixes.

--- Aid/Collection/CollectionExtension.cs
+++ Aid/Collection/CollectionExtension.cs
@@ -5,7 +5,9 @@ using System.Linq;
 namespace Software9119.Aid.Collection
 {
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
   public static class CollectionExtension
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
   {
 
     /// <summary>
