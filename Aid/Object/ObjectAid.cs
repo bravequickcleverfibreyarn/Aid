36f80933929001846f5f846666063a6745e409cb
Jiří <Jiri.Cepelka@Volny.cz>
2020-04-18 21:24:14 +0200

    Object aid.

---
+++ Aid/Object/ObjectAid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Software9119.Aid.Object
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+  static public class ObjectAid
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+  {
+    /// <summary>
+    /// Null for null <paramref name="obj"/>. Else <paramref name="func"/> result on <paramref name="obj"/>.
+    /// </summary>        
+    static public U NullOrResult<T, U>(T obj, Func<T, U> func)
+    where T : class
+    where U : class
+    {
+      ThrowNullFunc(func, nameof(func));
+
+      return obj.IsNull() ? null : func(obj);
+    }
+
+    /// <summary>
+    /// Nullable <typeparamref name="U"/>> for null <paramref name="obj"/>. Else <paramref name="func"/> result on <paramref name="obj"/>.
+    /// </summary>  
+    static public U? NullableOrResult<T, U>(T obj, Func<T, U> func)
+    where T : class
+    where U : struct
+    {
+      ThrowNullFunc(func, nameof(func));
+
+      return obj.IsNull() ? (U?)null : func(obj);
+    }
+
+    static void ThrowNullFunc(object func, string paramName)
+    {
+      if (func.IsNull())
+      {
+        throw new ArgumentNullException(paramName);
+      }
+    }
+  }
+}
---
+++ Test/ObjectAidTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Software9119.Aid.Object;
+using System;
+
+namespace Test
+{
+  [TestClass]
+  public class ObjectAidTests
+  {
+    // NullOrResult tests
+
+    [TestMethod]
+    public void NullOrResult_NullProvided_ReturnsNull()
+    {
+      Assert.IsNull(ObjectAid.NullOrResult(null, (string str) => str));
+    }
+
+    [TestMethod]
+    public void NullOrResult_ValueProvided_ReturnsFunctionResult()
+    {
+      Func<string, string> func = str => str.Substring(2);
+      string value = "XYZ";
+
+      Assert.AreEqual(func(value), ObjectAid.NullOrResult(value, func));
+    }
+
+    [TestMethod]
+    public void NullOrResult_NullFunc_ThrowsArgNullException()
+    {
+      Assert.ThrowsException<ArgumentNullException>(() => ObjectAid.NullOrResult<string, object>(null, null));
+    }
+
+    // NullableOrResult tests
+
+    [TestMethod]
+    public void NullableOrResult_NullProvided_ReturnsNullable()
+    {
+      Assert.AreEqual(default(char?), ObjectAid.NullableOrResult(null, (string str) => str[0]));
+    }
+
+    [TestMethod]
+    public void NullableOrResult_ValueProvided_ReturnsFunctionResult()
+    {
+      Func<string, char> func = str => str[1];
+      string value = "XYZ";
+
+      Assert.AreEqual(func(value), ObjectAid.NullableOrResult(value, func));
+    }
+
+    [TestMethod]
+    public void NullableOrResult_NullFunc_ThrowsArgNullException()
+    {
+      Assert.ThrowsException<ArgumentNullException>(() => ObjectAid.NullableOrResult<string, int>(null, null));
+    }
+  }
+}
