a4be2e574cdd776fdbe84fe3fcf0ec6d9133a8fe
Jiří <Jiri.Cepelka@Volny.cz>
2021-01-23 14:25:39 +0100

    WaitHandle WaitOneAsync

---
+++ Aid/Parallel/WaitHandleExtensions.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Software9119.Aid.Parallel
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+  static public class WaitHandleExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+  {
+    /// <summary>
+    /// Provides <see cref="WaitHandle"/> with task-pattern base asynchronicity.
+    /// </summary>    
+    public static Task<bool> WaitOneAsync(this WaitHandle waitHandle, int? timeoutMilisecs = null)
+    {
+      if (timeoutMilisecs is null)
+      {
+        timeoutMilisecs = -1; // Infinite.
+      }
+
+      var tcs = new TaskCompletionSource<bool>();
+
+      RegisteredWaitHandle rwh = ThreadPool.RegisterWaitForSingleObject
+      (
+          waitHandle,
+          (_, timedOut) => tcs.SetResult(!timedOut),
+          null,
+          timeoutMilisecs.Value,
+          true);
+
+      tcs.Task.ContinueWith(_ => rwh.Unregister(null), TaskScheduler.Default);
+      return tcs.Task;
+    }
+  }
+
+}
---
+++ Test/Parallel/WaitHandleExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Software9119.Aid.Parallel;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test.Parallel
+{
+  [TestClass]
+  public class WaitHandleExtensionsTests  
+  {
+    [TestMethod]
+    public void WaitOneAsync_NormalUsageEventNotSet_AsyncBehavior()
+    {
+      WaitHandle eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+      Task<bool> task = eventWaitHandle.WaitOneAsync();
+      Thread.Sleep(400);
+
+      Assert.AreEqual(TaskStatus.WaitingForActivation, task.Status);
+
+      ((EventWaitHandle)eventWaitHandle).Set();
+      Thread.Sleep(400);
+
+      Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+      Assert.AreEqual(true, task.Result);
+    }
+
+    [TestMethod]
+    public void WaitOneAsync_NormalUsageEventSet_AsyncBehaviorNoWait()
+    {
+      WaitHandle eventWaitHandle = new EventWaitHandle(true, EventResetMode.AutoReset);
+
+      Task<bool> task = eventWaitHandle.WaitOneAsync();
+      Thread.Sleep(400);
+
+      Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);     
+      Assert.AreEqual(true, task.Result);
+    }
+
+    [TestMethod]
+    public void WaitOneAsync_TimeoutSetEventNotSet_TimedOut()
+    {
+      WaitHandle eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+      Task<bool> task = eventWaitHandle.WaitOneAsync(1_000);
+      Thread.Sleep(400);
+
+      Assert.AreEqual(TaskStatus.WaitingForActivation, task.Status);
+
+      task.ContinueWith
+      (
+        x =>
+        {
+          Assert.AreEqual(TaskStatus.RanToCompletion, x.Status);
+          Assert.AreEqual(false, task.Result);
+        }
+      );
+    }
+
+    [TestMethod]
+    public void WaitOneAsync_TimeoutSetEventSet_AsyncBehaviorNoWait()
+    {
+      WaitHandle eventWaitHandle = new EventWaitHandle(true, EventResetMode.AutoReset);
+
+      Task<bool> task = eventWaitHandle.WaitOneAsync(1_000);
+      Thread.Sleep(100);
+
+      Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+      Assert.AreEqual(true, task.Result);
+    }
+
+  }
+}
