54703ad8314d45e3f768d35fda7aeaf9361abeac
Jiří <Jiri.Cepelka@Volny.cz>
2021-12-30 14:18:50 +0100

    Minuteness

--- Aid/Type/TypeAide.cs
+++ Aid/Type/TypeAide.cs
@@ -5,42 +5,41 @@ using System.Reflection;
 
 using sType = System.Type;
 
-namespace Software9119.Aid.Type
+namespace Software9119.Aid.Type;
+
+static public class TypeAide
 {
-  static public class TypeAide
+  static readonly ConcurrentDictionary<sType, bool> cache = new();
+
+  static public bool IsUnManaged ( sType type )
   {
-    static readonly ConcurrentDictionary<sType, bool> cache = new();
-
-    static public bool IsUnManaged(sType type)
-    {      
-      if (type == null)
-      {
-        throw new ArgumentNullException(nameof(type));
-      }
-
-      if (cache.ContainsKey(type))
-      {
-        return cache[type];
-      }
-
-      bool unmanaged = false;
-      if (type.IsPrimitive || type.IsPointer || type.IsEnum)
-      {
-        unmanaged = true;
-      }
-      else if (!type.IsValueType)
-      {
-        unmanaged = false;
-      }      
-      else
-      {
-        unmanaged = type
-          .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-          .All(fi => IsUnManaged(fi.FieldType));
-      }
-
-      _ = cache.TryAdd(type, unmanaged);
-      return unmanaged;
+    if (type == null)
+    {
+      throw new ArgumentNullException (nameof (type));
     }
+
+    if (cache.ContainsKey (type))
+    {
+      return cache [type];
+    }
+
+    bool unmanaged = false;
+    if (type.IsPrimitive || type.IsPointer || type.IsEnum)
+    {
+      unmanaged = true;
+    }
+    else if (!type.IsValueType)
+    {
+      unmanaged = false;
+    }
+    else
+    {
+      unmanaged = type
+        .GetFields (BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+        .All (fi => IsUnManaged (fi.FieldType));
+    }
+
+    _ = cache.TryAdd (type, unmanaged);
+    return unmanaged;
   }
 }
